e34a0adc5467e18904b4a38f02ce6bd27df4bc48
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2020-03-26 11:15:15 +0100

    Tilemaps + karaktär
    Den här lektionen gjorde jag ett litet test ställe för min karaktär och min tilemap. Testade collider och rigidbody samt hur man ska röra sig

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float controlX = Input.GetAxisRaw("Horizontal");
+        float controlY = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(controlX);
+        Debug.Log(controlY);
+
+        Vector3 movementX = Vector3.right * Time.deltaTime * speed * controlX;
+        
+
+        transform.Translate(movementX);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 movementY = Vector3.up * Time.deltaTime * speed * controlY;
+            transform.Translate(movementY);
+        }
+        
+    }
+}
