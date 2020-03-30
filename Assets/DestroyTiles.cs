3291d40b435f8ad632e03eecd2f9f5af909377c5
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2020-03-30 16:31:50 +0200

    Ta sönder Tiles
    Det här scriptet gör att man kan ta sönder tiles genom att hålla över och klicka på den. Den använder positionen av musen och sedan WorldToCell för att kolla vilken ruta man har musen i. Sedan använder den positionen av musen och sätter den cell som man har musen över till Null, vilket är inget.

---
+++ Assets/DestroyTiles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DestroyTiles : MonoBehaviour
+{
+
+    Tilemap Block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Block = GameObject.Find("Karta");
+        Block = GameObject.Find("Karta").GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        Vector3 MusPos = Input.mousePosition;
+        Debug.Log(MusPos);
+        if (Input.GetMouseButton(0))
+        {
+            Vector3Int TilePos = Tilemap.WorldToCell(MusPos);
+            Block.SetTile(TilePos, null);
+        }
+        */
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 SnällaFungera = Camera.current.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(SnällaFungera);
+
+            Vector3Int TilePos = Block.WorldToCell(SnällaFungera);
+            Debug.Log(SnällaFungera);
+
+            Block.SetTile(TilePos, null);
+        }
+    }
+}
