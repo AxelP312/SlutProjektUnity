b0a4a4e41dad530a2da0fa8c818adc09db6a2906
AxelP312 <60465685+AxelP312@users.noreply.github.com>
2020-04-01 11:09:05 +0200

    Skapa tile script!!
    I det här scriptet gjorde jag i princip likadant som i DestroyTiles scriptet.
    Den ända skillnanden är att jag bytte ut Null mot en tile som jag gjorde genom att skapa en public Tile som jag sedan drog in en sprite i. Dock har jag problemet att jag endast kan använda en förbestämd sprite.

---
+++ Assets/CreateTiles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CreateTiles : MonoBehaviour
+{
+    Tilemap Block;
+    public Tile Test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Block = GameObject.Find("Karta").GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Högerklick = 1
+        if(Input.GetMouseButton(1))
+        {
+            Vector3 MusPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(MusPos);
+
+            Vector3Int TilePos = Block.WorldToCell(MusPos);
+            Debug.Log(MusPos);
+
+            //BYT Test MOT EN TILE
+            Block.SetTile(TilePos, Test);
+        }
+    }
+}
